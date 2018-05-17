532449c154ad3db15accc8097e1ea950908494b4
Stephen <wizebin@gmail.com>
2018-05-17 00:25:48 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import { wrapAction } from 'nexusdk';
+import fetch from 'react-native-digest-fetch';
+
+
+wrapAction((properties, sendMessage) => {
+  const { verb, url, headers, body } = properties;
+  return new Promise(resolve => {
+    const result = fetch(url, {
+      method: verb,
+      headers,
+      body: verb !== 'GET' ? body : undefined,
+    });
+
+    result.then(data => {
+      resolve(data.text());
+    });
+  });
+});
+
